08ce2bb8b462cf7778c912ba3f7ee1521ea80848
ZonicS1615 <zonic.s1615@gmail.com>
2026-02-05 11:15:06 +0700

    workshop

---
+++ Assets/Scirpts/Utils/Lifetime.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Lifetime : MonoBehaviour
+{
+    [SerializeField] private float lifetime = 1f;
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
+
